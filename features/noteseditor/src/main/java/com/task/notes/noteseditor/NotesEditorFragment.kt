8339768c13950a8d085da4d48bf2419729956a03
Abdul-Mujib Aliu <aliuabdulmujib@gmail.com>
2021-06-19 12:06:05 +0100

    Added notes editor UI and viewbinding extensions

--- features/noteseditor/src/main/java/com/task/notes/noteseditor/NotesEditorFragment.kt
+++ features/noteseditor/src/main/java/com/task/notes/noteseditor/NotesEditorFragment.kt
@@ -5,21 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.task.notes.noteseditor.ext.viewBinding
 import com.task.notesapp.noteseditor.R
+import com.task.notesapp.noteseditor.databinding.FragmentNotesEditorBinding
 
-class NotesEditorFragment : Fragment() {
+class NotesEditorFragment : Fragment(R.layout.fragment_notes_editor) {
+
+    private val binding by viewBinding(FragmentNotesEditorBinding::bind)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notes_editor, container, false)
-    }
 
 }
