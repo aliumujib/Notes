7c85435af14d1ced1be4afc12b8ae8f593f45a92
Abdul-Mujib Aliu <aliuabdulmujib@gmail.com>
2021-06-21 20:42:17 +0100

    Pre merge backup

--- features/noteslist/src/test/java/com/task/notes/noteslist/domain/StreamNotesUseCaseTest.kt
+++ features/noteslist/src/test/java/com/task/notes/noteslist/domain/StreamNotesUseCaseTest.kt
@@ -4,26 +4,23 @@ import com.task.notes.repository.NotesRepository
 import com.task.notes.testsharedutils.TestPostExecutionThreadImpl
 import io.mockk.MockKAnnotations
 import io.mockk.impl.annotations.MockK
-import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.After
 import org.junit.Before
 
-import org.junit.Assert.*
-
+@ExperimentalCoroutinesApi
 class StreamNotesUseCaseTest {
 
     @MockK
     lateinit var notesRepository: NotesRepository
 
-    lateinit var sut : StreamNotesUseCase
+    lateinit var sut: StreamNotesUseCase
 
     @Before
     fun setUp() {
         MockKAnnotations.init(this, relaxed = true)
         sut = StreamNotesUseCase(notesRepository, TestPostExecutionThreadImpl())
     }
-
-
 
     @After
     fun tearDown() {
